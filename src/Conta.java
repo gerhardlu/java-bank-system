bbc0f04afe1b7593a784dc639e320350dfd4d4e0
Gehard <130104770+gerhardlu@users.noreply.github.com>
2026-07-19 15:53:10 -0300

    Uma classe é um molde. conta vai ser o molde de "o que é uma conta bancaria" e quais informações ela guarda e o que ela sabe fazer

---
+++ src/Conta.java
@@ -0,0 +1,48 @@
+public class Conta {
+
+    // atributs: as informações que toda conta precisa ter
+    private String titular;
+    private int numero;
+    private double saldo;
+
+    // construtos: como uma conta "nasce"
+    public Conta(String titular,  int numero)  {
+        this.titular = titular;
+        this.numero = numero;
+        this.saldo = 0; // toda conta começar com saldo zero
+    }
+
+    // Método: depositar dinheiro
+    public void depositar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor de depósito inválido");
+            return;
+        }
+        this.saldo += valor;
+        System.out.println("Depósito de R$ " + valor + " realizado com sucesso");
+
+    }
+
+    // Método: sacar dinheiro
+    public void sacar(double valor) {
+        if (valor <= 0) {
+            System.out.println("Valor de saque inválido");
+            return;
+        }
+        if (valor > this.saldo) {
+            System.out.println("Saldo insuficiente");
+            return;
+        }
+        this.saldo -= valor;
+        System.out.println("Saque de R$ " + valor + "realizado com sucesso");
+    }
+
+    // Método: exibir o saldo atual
+    public void verSaldo() {
+        System.out.println("Titular: " + titular);
+        System.out.println("Conta: " + numero);
+        System.out.println("Saldo atual: " + saldo);
+    }
+
+
+}
