0cdb0e40dcedc4be8f45193d69ffcf3a24daa891
Gehard <130104770+gerhardlu@users.noreply.github.com>
2026-07-20 16:36:25 -0300

    implementa classe Conta com despósito, saque e validações

---
+++ src/screenmatch.java
@@ -0,0 +1,8 @@
+public class screenmatch {
+    public static void main(String[] args) {
+        System.out.println("Esse é o Screen Match");
+        System.out.println("Filme: War Machine");
+        int ano = 2026;
+
+    }
+}
